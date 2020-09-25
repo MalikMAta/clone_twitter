47d1b8745be54d4abe6f6af2077b3502f60da622
Malik Ata <malikmajedata@gmail.com>
2020-09-25 09:43:40 -0700

    material_ui

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 
 function App() {
   return (
+    // BEM
+
     <div className="App">
+       <h1>Malik Ata Lets build</h1>
+
+       {/*  Sidebar */}
+
+       {/*  Feed */}
+
+       {/*  Widgets */}
        
-     <h1>Malik Ata Lets build</h1>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './Sidebar.css'
+import { Button } from '@material-ui/core';
+
+
+function Sidebar (){
+
+
+    return(
+        <div className = "sidebar">
+
+            {/* Twitter Icon */}
+
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+
+            { /* Button -> Tweet */}
+        </div>
+    )
+}
